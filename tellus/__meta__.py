b7955161b87db6e4bc19dfd40934ce6a5491a7ed
wakamezake <hotwater1367@gmail.com>
2022-03-21 11:13:58 +0900

    set meta info

--- tellus/__meta__.py
+++ tellus/__meta__.py
@@ -1,12 +1,12 @@
 # `name` is the name of the package as used for `pip install package`
-name = "package-name"
+name = "tellus"
 # `path` is the name of the package for `import package`
 path = name.lower().replace("-", "_").replace(" ", "_")
 # Your version number should follow https://python.org/dev/peps/pep-0440 and
 # https://semver.org
 version = "0.1.dev0"
-author = "Author Name"
-author_email = ""
-description = ""  # One-liner
-url = ""  # your project homepage
+author = "Kota Yuhara"
+author_email = "hotwater1367@gmail.com"
+description = "Python client for Tellus API"  # One-liner
+url = "https://github.com/wakame1367/tellus_client"  # your project homepage
 license = "MIT"  # See https://choosealicense.com
